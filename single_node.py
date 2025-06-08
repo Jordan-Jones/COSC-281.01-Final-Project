0271e5fabd6dd9afa7990954f7a8bc62e520a51f
Jordan Jones <Jordan.M.Jones.24@dartmouth.edu>
2025-06-07 23:51:31 -0400

    forgot to add something

--- single_node.py
+++ single_node.py
@@ -10,6 +10,9 @@ def main():
     mapper_node = MapperNode()
     search_node = SearchMaze()
 
+    # Join the two nodes using a MultiThreadedExecutor
+
+    # Have the occupancy grid map published by the mapper node be subscribed to by the search node
     executor = MultiThreadedExecutor()
     executor.add_node(mapper_node)
     executor.add_node(search_node)
